cd7acc953378d17c3cce2be38855579627246972
kylinmod <kylinmod@gmail.com>
2013-12-26 18:52:31 +0800

    NavigationBar: Custom height (2/2)

--- src/com/android/settings/kylinmod/KylinNavbar.java
+++ src/com/android/settings/kylinmod/KylinNavbar.java
@@ -16,19 +16,57 @@
 
 package com.android.settings.kylinmod;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class KylinNavbar extends SettingsPreferenceFragment {
+public class KylinNavbar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "KylinNavbar";
+	
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+    private ListPreference mNavButtonsHeight;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         addPreferencesFromResource(R.xml.navbar_settings);
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+		//Navigation Bar Height
+        mNavButtonsHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavButtonsHeight.setOnPreferenceChangeListener(this);
+
+        int statusNavButtonsHeight = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavButtonsHeight.setValue(String.valueOf(statusNavButtonsHeight));
+        mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntry());
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getApplicationContext().getContentResolver();
+        if (preference == mNavButtonsHeight) {
+            int index = mNavButtonsHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(resolver, Settings.System.NAVIGATION_BAR_HEIGHT, 
+                    Integer.valueOf((String) objValue));
+            mNavButtonsHeight.setSummary(mNavButtonsHeight.getEntries()[index]);
+            return true;
+        }
+		
+        return false;
     }
 }

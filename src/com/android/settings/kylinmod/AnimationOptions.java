25976714486a9ff9b2b5f4945d22b509d7a75b2c
kylinmod <kylinmod@gmail.com>
2014-01-22 13:30:56 +0800

    ElectronBeam: Add an alternative screen-off animation (2/2)

--- src/com/android/settings/kylinmod/AnimationOptions.java
+++ src/com/android/settings/kylinmod/AnimationOptions.java
@@ -16,6 +16,8 @@
 
 package com.android.settings.kylinmod;
 
+import static android.provider.Settings.System.SCREEN_OFF_ANIMATION;
+
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -35,9 +37,15 @@ public class AnimationOptions extends SettingsPreferenceFragment  implements
     private static final String KEY_LISTVIEW_ANIMATION = "listview_animation";
     private static final String KEY_LISTVIEW_INTERPOLATOR = "listview_interpolator";
 
+    // Screen Off Animation Key
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
+
     // ListView Animations Preference
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
+
+    // Screen Off Animation
+    private ListPreference mScreenOffAnimationPreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,7 +68,32 @@ public class AnimationOptions extends SettingsPreferenceFragment  implements
         mListViewInterpolator.setValue(String.valueOf(listviewinterpolator));
         mListViewInterpolator.setSummary(mListViewInterpolator.getEntry());
         mListViewInterpolator.setOnPreferenceChangeListener(this);
+
+        mScreenOffAnimationPreference = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        final int currentAnimation = Settings.System.getInt(resolver, SCREEN_OFF_ANIMATION,
+                1 /* CRT-off */);
+        mScreenOffAnimationPreference.setValue(String.valueOf(currentAnimation));
+        mScreenOffAnimationPreference.setOnPreferenceChangeListener(this);
+        updateScreenOffAnimationPreferenceDescription(currentAnimation);
     }
+
+    private void updateScreenOffAnimationPreferenceDescription(int currentAnim) {
+        ListPreference preference = mScreenOffAnimationPreference;
+        String summary;
+        if (currentAnim < 0) {
+            // Unsupported value
+            summary = "";
+        } else {
+            final CharSequence[] entries = preference.getEntries();
+            final CharSequence[] values = preference.getEntryValues();
+            if (entries == null || entries.length == 0) {
+                summary = "";
+            } else {
+                summary = entries[currentAnim].toString();
+            }
+        }
+        preference.setSummary(summary);
+   }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         ContentResolver resolver = getContentResolver();
@@ -77,6 +110,16 @@ public class AnimationOptions extends SettingsPreferenceFragment  implements
             Settings.System.putInt(resolver, Settings.System.LISTVIEW_INTERPOLATOR,
                     listviewinterpolator);
             mListViewInterpolator.setSummary(mListViewInterpolator.getEntries()[index]);
+            return true; 
+        }
+        } else if (KEY_SCREEN_OFF_ANIMATION.equals(key)) {
+            int value = Integer.parseInt((String) objValue);
+            try {
+                Settings.System.putInt(getContentResolver(), SCREEN_OFF_ANIMATION, value);
+                updateScreenOffAnimationPreferenceDescription(value);
+            } catch (NumberFormatException e) {
+                Log.e(TAG, "could not persist screen-off animation setting", e);
+            }
             return true; 
         }
 

4457a6d8031817c8e8454335666a02374e85eb39
kylinmod <kylinmod@gmail.com>
2014-01-30 19:54:18 +0800

    Settings: add Ram bar and news general interface (1/2)

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -1213,4 +1213,5 @@ public class Settings extends PreferenceActivity
     public static class ApnEditorActivity extends Settings { /* empty */ }
     public static class BlacklistSettingsActivity extends Settings { /* empty */ }
     public static class SystemSettingsActivity extends Settings { /* empty */ }
+    public static class ASSRamBarActivity extends Settings { /* empty */ } 
 }
